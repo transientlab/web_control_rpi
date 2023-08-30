120dd886cb3bf0754d9859a98e475cb6864a60a8
kr315 <kreispawel@gmail.com>
2023-08-30 21:19:33 +0200

    remove wake_pc bugfixes in configure_server.sh

--- configure_server.sh
+++ configure_server.sh
@@ -2,7 +2,7 @@
 sudo apt-get install curl wget wakeonlan git &&
 
 # instal mqtt tools
-sudo apt-get install mosquitto mosquitto-client &&
+sudo apt-get install mosquitto mosquitto-clients &&
 
 # node.js server
 sudo apt-get install npm && sudo npm install express &&
@@ -14,21 +14,21 @@ sudo apt-get install openocd gcc-arm-none-eabi minicom &&
 sudo apt-get install build-essential &&
 
 # camera capture
-sudo apt-get install ffmpeg &&
+sudo apt-get install ffmpeg
 
 # airplay https://github.com/mikebrady/shairport-sync
-# cd ~ &&
-# sudo apt-get install autoconf libtool libdaemon-dev libasound2-dev libpopt-dev libconfig-dev &&
-# sudo apt install avahi-daemon libavahi-client-dev &&
-# sudo apt install libssl-dev &&
-# git clone https://github.com/mikebrady/shairport-sync.git &&
-# cd shairport-sync &&
-# autoreconf -i -f &&
-# ./configure --with-alsa --with-avahi --with-ssl=openssl --with-systemd --with-metadata &&
-# make &&
-# sudo make install &&
-# sudo systemctl enable shairport-sync &&
-# sudo service shairport-sync start
+cd ~ &&
+sudo apt-get install autoconf libtool libdaemon-dev libasound2-dev libpopt-dev libconfig-dev &&
+sudo apt install avahi-daemon libavahi-client-dev &&
+sudo apt install libssl-dev &&
+git clone https://github.com/mikebrady/shairport-sync.git &&
+cd shairport-sync &&
+autoreconf -i -f &&
+./configure --with-alsa --with-avahi --with-ssl=openssl --with-systemd --with-metadata &&
+make &&
+sudo make install &&
+sudo systemctl enable shairport-sync &&
+sudo service shairport-sync start
 
 # airplay config available: https://pimylifeup.com/raspberry-pi-airplay-receiver/
 
--- wake_pc.sh
+++
@@ -1,1 +0,0 @@
-wakeonlan e0:d5:5e:41:6a:65
